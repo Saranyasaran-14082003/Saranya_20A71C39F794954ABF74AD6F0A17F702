35f8e7b336893650ddb0149897acbd7b02f5d67f
Saranyasaran-14082003 <144700907+Saranyasaran-14082003@users.noreply.github.com>
2023-09-11 21:08:49 +0530

    Add files via upload

---
+++ challenge -2/main (1).py
@@ -0,0 +1,9 @@
+>>> #check the second condition by taking negative number.
+>>> num = int(input (" Enter a number: "))
+Enter a number : -5
+>>> if num < 0:
+  print ("Sorry, factorial does not exist for negative numbers ")
+elif num == 0 : 
+  print (" the factorial of 0 is 1")
+else:
+  print ("the factorial of",is",recur_factorial(num))
